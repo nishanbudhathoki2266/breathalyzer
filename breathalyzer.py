7f2479b31f4892cf0aa13a16b9d9824857c13b27
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-12-17 13:52:18 +0545

    Implemented the program in a loop (Runs forever)

--- breathalyzer.py
+++ breathalyzer.py
@@ -9,15 +9,21 @@ GPIO.setup(digitalPin, GPIO.IN)
 GPIO.setup(ledPin, GPIO.OUT)
 
 
-alcoholLevel = GPIO.input(digitalPin)
+# Runing in loop - Main loop
 
-if alcoholLevel == 1:
+while True:
+    alcoholLevel = GPIO.input(digitalPin)
+    if alcoholLevel == 1:
         # Sober
         GPIO.output(ledPin, GPIO.HIGH)
     else:
         # Drunk
-        for _ in range(5):  # Blink 5 times
-            GPIO.output(ledPin, GPIO.HIGH)
-            time.sleep(0.5)
-            GPIO.output(ledPin, GPIO.LOW)
-            time.sleep(0.5)
+            for _ in range(5):  # Blink 5 times
+                GPIO.output(ledPin, GPIO.HIGH)
+                time.sleep(0.5)
+                GPIO.output(ledPin, GPIO.LOW)
+                time.sleep(0.5)
+    
+    
+
+
