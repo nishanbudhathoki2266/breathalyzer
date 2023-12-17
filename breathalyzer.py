8f6d214b69b137f85f0c047788c8586695e087ed
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-12-17 13:53:17 +0545

    Added a little of opmizations using try except

--- breathalyzer.py
+++ breathalyzer.py
@@ -11,19 +11,25 @@ GPIO.setup(ledPin, GPIO.OUT)
 
 # Runing in loop - Main loop
 
-while True:
-    alcoholLevel = GPIO.input(digitalPin)
-    if alcoholLevel == 1:
-        # Sober
-        GPIO.output(ledPin, GPIO.HIGH)
-    else:
-        # Drunk
-            for _ in range(5):  # Blink 5 times
-                GPIO.output(ledPin, GPIO.HIGH)
-                time.sleep(0.5)
-                GPIO.output(ledPin, GPIO.LOW)
-                time.sleep(0.5)
-    
+try:
+    while True:
+        alcoholLevel = GPIO.input(digitalPin)
+        if alcoholLevel == 1:
+            # Sober
+            GPIO.output(ledPin, GPIO.HIGH)
+        else:
+            # Drunk
+                for _ in range(5):  # Blink 5 times
+                    GPIO.output(ledPin, GPIO.HIGH)
+                    time.sleep(0.5)
+                    GPIO.output(ledPin, GPIO.LOW)
+                    time.sleep(0.5)
+
+except KeyboardInterrupt:
+    pass
+
+finally:
+    GPIO.cleanup()
     
 
 
