b97318866c4dabcf3c9b0850fb8ef5863e642c4a
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-12-17 13:55:30 +0545

    Refactored the logics into functions

--- breathalyzer.py
+++ breathalyzer.py
@@ -8,22 +8,29 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(digitalPin, GPIO.IN)
 GPIO.setup(ledPin, GPIO.OUT)
 
+# Function to read alcohol level
+def readAlcoholLevel():
+    return GPIO.input(digitalPin)
+
+# Function to control LED based on alcohol level
+def controlLed(alcoholLevel):
+    if alcoholLevel == 1:
+        # Sober
+        GPIO.output(led_pin, GPIO.HIGH)
+    else:
+        # Drunk
+        for _ in range(5):  # Blink 5 times
+            GPIO.output(led_pin, GPIO.HIGH)
+            time.sleep(0.5)
+            GPIO.output(led_pin, GPIO.LOW)
+            time.sleep(0.5)
 
 # Runing in loop - Main loop
-
 try:
     while True:
-        alcoholLevel = GPIO.input(digitalPin)
-        if alcoholLevel == 1:
-            # Sober
-            GPIO.output(ledPin, GPIO.HIGH)
-        else:
-            # Drunk
-                for _ in range(5):  # Blink 5 times
-                    GPIO.output(ledPin, GPIO.HIGH)
-                    time.sleep(0.5)
-                    GPIO.output(ledPin, GPIO.LOW)
-                    time.sleep(0.5)
+        alcoholLevel = readAlcoholLevel()
+        controlLed(alcoholLevel)
+        time.sleep(2)
 
 except KeyboardInterrupt:
     pass
